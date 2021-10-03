ffd8578a9346db838fd9f5059387f45e09a86218
dogukanermis <ermisd+github@pm.me>
2021-10-03 15:04:56 +0200

    create first table

--- github-file-list/src/index.js
+++ github-file-list/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function FileList({ files }) {
+  return (
+    <table>
+      <tbody>
+        {files.map((file) => (
+          <tr key={file.id}>
+            <td>{file.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: '2016-07-11 21:24:00',
+    latestCommit: { message: 'Initial commit' },
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: '2016-07-11 21:24:00',
+    latestCommit: { message: 'Initial commit' },
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: '2016-07-18 21:24:00',
+    latestCommit: { message: 'Added a readme' },
+  },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FileList files={testFiles} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
