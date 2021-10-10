21ae4846e9fe0b768662e03a4073fc517eb1174e
dogukanermis <ermisd+github@pm.me>
2021-10-10 14:04:37 +0200

    add class component

--- states/src/index.js
+++ states/src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class CountingParent extends React.Component {
+  // The constructor is called when a
+  // component is created
+  constructor(props) {
+    super(props);
+    // Set the state here. Use "props" if needed.
+    this.state = { actionCount: 0 };
+
+    // Bind the event handler function, so that its
+    // `this` binding isn't lost when it gets passed
+    // to the button
+    this.handleAction = this.handleAction.bind(this);
+  }
+
+  handleAction(action) {
+    console.log('Child says', action);
+    // Replace actionCount with an incremented value
+    this.setState({ actionCount: this.state.actionCount + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child onAction={this.handleAction} />
+        <p>Clicked {this.state.actionCount} times</p>
+      </div>
+    );
+  }
+}
+
+function Child({ onAction }) {
+  return <button onClick={onAction}>Click Me!</button>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CountingParent />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
