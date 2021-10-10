e2597372fc6c07e8f9126376630adae671718239
dogukanermis <ermisd+github@pm.me>
2021-10-10 14:39:41 +0200

    refactor parent class

--- states/src/index.js
+++ states/src/index.js
@@ -3,22 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class CountingParent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { actionCount: 0 };
+  state = { actionCount: 0 };
 
-    this.handleAction = this.handleAction.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
-
-  handleAction(action) {
+  handleAction = (action) => {
     console.log('Child says', action);
     this.setState({ actionCount: this.state.actionCount + 1 });
-  }
+  };
 
-  handleReset() {
+  handleReset = () => {
     this.setState({ actionCount: 0 });
-  }
+  };
 
   render() {
     return (
