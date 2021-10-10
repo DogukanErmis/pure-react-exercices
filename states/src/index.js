2d30bc47d5f6b8071376802a8d6b90ef0bda855e
dogukanermis <ermisd+github@pm.me>
2021-10-10 14:05:46 +0200

    remove comments

--- states/src/index.js
+++ states/src/index.js
@@ -3,22 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class CountingParent extends React.Component {
-  // The constructor is called when a
-  // component is created
   constructor(props) {
     super(props);
-    // Set the state here. Use "props" if needed.
     this.state = { actionCount: 0 };
 
-    // Bind the event handler function, so that its
-    // `this` binding isn't lost when it gets passed
-    // to the button
     this.handleAction = this.handleAction.bind(this);
   }
 
   handleAction(action) {
     console.log('Child says', action);
-    // Replace actionCount with an incremented value
     this.setState({ actionCount: this.state.actionCount + 1 });
   }
 
