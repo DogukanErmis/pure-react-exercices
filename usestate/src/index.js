c00fbdbf91ed9f6db3ca65197507aaa383eb109f
dogukanermis <ermisd+github@pm.me>
2021-10-16 17:20:50 +0200

    remove comments

--- usestate/src/index.js
+++ usestate/src/index.js
@@ -1,26 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-// Class component
-// class OneTimeButton extends React.Component {
-//   state = {
-//     clicked: false,
-//   };
-
-//   handleClick = () => {
-//     this.props.onClick();
-//     this.setState({ clicked: true });
-//   };
-
-//   render() {
-//     return (
-//       <button onClick={this.handleClick} disabled={this.state.clicked}>
-//         You can only click me once
-//       </button>
-//     );
-//   }
-// }
 
 function OneTimeButton({ onClick }) {
   const [clicked, setClicked] = React.useState(false);
