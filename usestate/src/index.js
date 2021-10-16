2bad3f1dd220c7e6276f6cfe8f851abf939c4882
dogukanermis <ermisd+github@pm.me>
2021-10-16 17:12:34 +0200

    implement button logic

--- usestate/src/index.js
+++ usestate/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class OneTimeButton extends React.Component {
+  state = {
+    clicked: false,
+  };
+
+  handleClick = () => {
+    this.props.onClick();
+    this.setState({ clicked: true });
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick} disabled={this.state.clicked}>
+        You can only click me once
+      </button>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <OneTimeButton onClick={() => alert('hi')} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
