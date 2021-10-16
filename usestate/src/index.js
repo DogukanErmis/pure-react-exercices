2e615acf8d6521c2791ed5b6805e0355114e17ac
dogukanermis <ermisd+github@pm.me>
2021-10-16 17:18:21 +0200

    refactor class into function component

--- usestate/src/index.js
+++ usestate/src/index.js
@@ -2,23 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class OneTimeButton extends React.Component {
-  state = {
-    clicked: false,
+// Class component
+// class OneTimeButton extends React.Component {
+//   state = {
+//     clicked: false,
+//   };
+
+//   handleClick = () => {
+//     this.props.onClick();
+//     this.setState({ clicked: true });
+//   };
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick} disabled={this.state.clicked}>
+//         You can only click me once
+//       </button>
+//     );
+//   }
+// }
+
+function OneTimeButton({ onClick }) {
+  const [clicked, setClicked] = React.useState(false);
+
+  const handleClick = () => {
+    onClick();
+
+    setClicked(true);
   };
 
-  handleClick = () => {
-    this.props.onClick();
-    this.setState({ clicked: true });
-  };
-
-  render() {
-    return (
-      <button onClick={this.handleClick} disabled={this.state.clicked}>
-        You can only click me once
-      </button>
-    );
-  }
+  return (
+    <button onClick={handleClick} disabled={clicked}>
+      You can only click me once
+    </button>
+  );
 }
 
 ReactDOM.render(
