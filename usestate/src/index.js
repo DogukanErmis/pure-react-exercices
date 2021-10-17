ad9008e7253ff7071861e28acc019c277233a0c3
dogukanermis <ermisd+github@pm.me>
2021-10-17 16:41:01 +0200

    add increment exercices

--- usestate/src/index.js
+++ usestate/src/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function OneTimeButton({ onClick }) {
-  const [clicked, setClicked] = React.useState(false);
-
-  const handleClick = () => {
-    onClick();
-
-    setClicked(true);
-  };
+const MultiCounter = () => {
+  const [counts, setCounts] = useState({ countA: 0, countB: 0 });
+  const incA = () =>
+    setCounts((counts) => ({ ...counts, countA: counts.countA + 1 }));
+  const incB = () =>
+    setCounts((counts) => ({ ...counts, countB: counts.countB + 1 }));
+  const badIncA = () => setCounts({ countA: counts.countA + 1 });
 
   return (
-    <button onClick={handleClick} disabled={clicked}>
-      You can only click me once
-    </button>
+    <>
+      <div>A: {counts.countA}</div>
+      <div>B: {counts.countB}</div>
+      <button onClick={incA}>Increment A</button>
+      <button onClick={incB}>Increment B</button>
+      <button onClick={badIncA}>Increment A Badly</button>
+    </>
   );
-}
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <OneTimeButton onClick={() => alert('hi')} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<MultiCounter />, document.querySelector('#root'));
