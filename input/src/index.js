1e5dc3b935878b218afd1d594a3ad47f76e649b9
dogukanermis <ermisd+github@pm.me>
2021-10-23 16:48:33 +0200

    add exercice 1

--- input/src/index.js
+++ input/src/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+const InputNames = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const changeFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const changeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  return (
+    <>
+      <label>
+        Firstname:
+        <input type="text" value={firstName} onChange={changeFirstName} />
+      </label>
+      <label>
+        Lastname:
+        <input type="text" value={lastName} onChange={changeLastName} />
+      </label>
+      <p>
+        Hello {firstName} {lastName}!
+      </p>
+    </>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InputNames />
   </React.StrictMode>,
   document.getElementById('root')
 );
