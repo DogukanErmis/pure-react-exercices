f222f81dfb58dd8021c29cb0a83ed5e859a303c2
dogukanermis <ermisd+github@pm.me>
2021-10-26 17:17:50 +0200

    add useReducer hook to shopping list

--- use-reducer/src/index.js
+++ use-reducer/src/index.js
@@ -3,29 +3,44 @@ import ReactDOM from 'react-dom';
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'add':
+      return [...state, { id: state.length, name: action.name }];
+    default:
+      return state;
   }
-
-  function ShoppingList() {
-    const inputRef = useRef();
-    const [items, dispatch] = useReducer(reducer, []);
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <input ref={inputRef} />
-        </form>
-        <ul>
-          {items.map((item, index) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
-      </>
-    );
-  }
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
 };
+
+function ShoppingList() {
+  const inputRef = useRef();
+  const [items, dispatch] = useReducer(reducer, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (inputRef.current.value.trim() === '') return;
+
+    dispatch({ type: 'add', name: inputRef.current.value });
+
+    inputRef.current.value = '';
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} />
+      </form>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ShoppingList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
