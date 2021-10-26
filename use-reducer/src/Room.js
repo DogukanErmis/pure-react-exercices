f7e1013335138db673d6ac78e93a209818bac6fa
dogukanermis <ermisd+github@pm.me>
2021-10-26 18:02:06 +0200

    add room exercice

---
+++ use-reducer/src/Room.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react';
+import ReactDOM from 'react-dom';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'low':
+      return 'low';
+    case 'medium':
+      return 'medium';
+    case 'high':
+      return 'high';
+    case 'off':
+      return 'off';
+    default:
+      return state;
+  }
+};
+
+export const Room = () => {
+  const [light, dispatch] = useReducer(reducer, 'off');
+
+  return (
+    <>
+      <button onClick={() => dispatch({ type: 'low' })}>Low</button>
+      <button onClick={() => dispatch({ type: 'medium' })}>Medium</button>
+      <button onClick={() => dispatch({ type: 'high' })}>High</button>
+      <button onClick={() => dispatch({ type: 'off' })}>Off</button>
+      <p>{light}</p>
+    </>
+  );
+};
+
+ReactDOM.render(<Room />, document.getElementById('root'));
--- use-reducer/src/index.js
+++ use-reducer/src/index.js
@@ -1,56 +1,57 @@
 import React, { useReducer, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { Room } from './Room';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'add':
-      return [...state, { id: state.length, name: action.name }];
-    case 'remove': // keep every item except the one we want to remove
-      return state.filter((_, index) => index !== action.index);
-    case 'clear':
-      return [];
-    default:
-      return state;
-  }
-};
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'add':
+//       return [...state, { id: state.length, name: action.name }];
+//     case 'remove': // keep every item except the one we want to remove
+//       return state.filter((_, index) => index !== action.index);
+//     case 'clear':
+//       return [];
+//     default:
+//       return state;
+//   }
+// };
 
-function ShoppingList() {
-  const inputRef = useRef();
-  const [items, dispatch] = useReducer(reducer, []);
+// function ShoppingList() {
+//   const inputRef = useRef();
+//   const [items, dispatch] = useReducer(reducer, []);
 
-  function handleSubmit(e) {
-    e.preventDefault();
+//   function handleSubmit(e) {
+//     e.preventDefault();
 
-    if (inputRef.current.value.trim() === '') return;
+//     if (inputRef.current.value.trim() === '') return;
 
-    dispatch({ type: 'add', name: inputRef.current.value });
+//     dispatch({ type: 'add', name: inputRef.current.value });
 
-    inputRef.current.value = '';
-  }
+//     inputRef.current.value = '';
+//   }
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input ref={inputRef} />
-      </form>
-      <ul>
-        {items.map((item, index) => (
-          <li key={item.id}>
-            {item.name}
-            <button onClick={() => dispatch({ type: 'remove', index })}>
-              X
-            </button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={() => dispatch({ type: 'clear' })}>Clear</button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input ref={inputRef} />
+//       </form>
+//       <ul>
+//         {items.map((item, index) => (
+//           <li key={item.id}>
+//             {item.name}
+//             <button onClick={() => dispatch({ type: 'remove', index })}>
+//               X
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//       <button onClick={() => dispatch({ type: 'clear' })}>Clear</button>
+//     </>
+//   );
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    <ShoppingList />
+    <Room />
   </React.StrictMode>,
   document.getElementById('root')
 );
