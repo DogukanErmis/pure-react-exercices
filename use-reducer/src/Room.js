a13f36f50e4c4e7ab59f1255802ce9d645683873
dogukanermis <ermisd+github@pm.me>
2021-11-01 17:15:07 +0100

    install prettier

--- use-reducer/src/Room.js
+++ use-reducer/src/Room.js
@@ -1,19 +1,18 @@
 import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
-
 export const Room = () => {
-    const [light, dispatch] = useReducer(reducer, 'off');
+  const [light, dispatch] = useReducer(reducer, 'off');
 
-    return (
-        <>
-            <button onClick={() => dispatch({ type: 'low' })}>Low</button>
-            <button onClick={() => dispatch({ type: 'medium' })}>Medium</button>
-            <button onClick={() => dispatch({ type: 'high' })}>High</button>
-            <button onClick={() => dispatch({ type: 'off' })}>Off</button>
-            <p>{light}</p>
-        </>
-    );
+  return (
+    <>
+      <button onClick={() => dispatch({ type: 'low' })}>Low</button>
+      <button onClick={() => dispatch({ type: 'medium' })}>Medium</button>
+      <button onClick={() => dispatch({ type: 'high' })}>High</button>
+      <button onClick={() => dispatch({ type: 'off' })}>Off</button>
+      <p>{light}</p>
+    </>
+  );
 };
 
 const reducer = (state, action) => {
