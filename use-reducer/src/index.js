683a076557072b4d9a7fe334e11a053d3786fb4a
dogukanermis <ermisd+github@pm.me>
2021-11-02 16:26:01 +0100

    fetch reddit data dynamically

--- use-reducer/src/index.js
+++ use-reducer/src/index.js
@@ -21,7 +21,28 @@ function Reddit({ subreddit }) {
     </ul>
   );
 }
-ReactDOM.render(
-  <Reddit subreddit="reactjs" />,
-  document.querySelector('#root')
-);
+
+function App() {
+  // 2 pieces of state: one to hold the input value,
+  // another to hold the current subreddit.
+  const [inputValue, setValue] = useState('reactjs');
+  const [subreddit, setSubreddit] = useState(inputValue);
+
+  // Update the subreddit when the user presses enter
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setSubreddit(inputValue);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={inputValue} onChange={(e) => setValue(e.target.value)} />
+      </form>
+      <Reddit subreddit={subreddit} />
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
