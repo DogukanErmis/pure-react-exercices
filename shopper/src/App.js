a6f1fbde13748a63a9aca11010b73c10134c0de1
dogukanermis <ermisd+github@pm.me>
2021-11-18 16:05:06 +0100

    refactor item component

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 import './App.css';
 import ItemPage from './ItemPage';
+import CartPage from './CartPage';
 import { items } from './static-data';
 
 const summarizeCart = (cart) => {
@@ -22,25 +23,27 @@ const App = () => {
     setCart((prevCart) => [...prevCart, item]);
   };
 
-  console.log(cart);
-
   return (
     <div className="App">
       <Nav activeTab={activeTab} onTabChange={setActiveTab} />
       <main className="App-content">
-        <Content tab={activeTab} onAddToCart={addToCart} />
+        <Content
+          tab={activeTab}
+          onAddToCart={addToCart}
+          cart={summarizeCart(cart)}
+        />
       </main>
     </div>
   );
 };
 
-const Content = ({ tab, onAddToCart }) => {
+const Content = ({ tab, onAddToCart, cart }) => {
   switch (tab) {
     default:
     case 'items':
       return <ItemPage items={items} onAddToCart={onAddToCart} />;
     case 'cart':
-      return <span>the cart</span>;
+      return <CartPage items={cart} />;
   }
 };
 
